145bc91fa970ca6cc368de71e668d81c774c4b12
giwon_tecnet <ljh7a21@gmail.com>
2026-04-01 14:32:27 +0900

    fix:gitignore 수정

--- dns_manager.sh
+++ dns_manager.sh
@@ -47,7 +47,7 @@ do
         DNS_TYPE=$(get_dns_type)
         case $DNS_TYPE in
             master) echo "서버 타입   : Master" ;;
-            slave)  echo "서버 타입   : Slave (named.rfc1912.zones를 업데이트하려면 declupdate를 입력해주세요.)"  ;;
+            slave)  echo "서버 타입   : Slave (named.rfc1912.zones를 업데이트하려면 declupdate를 입력해주세요.)" ;;
             none)   echo "서버 타입   : none" ;;
             *)      echo "서버 타입   : 알 수 없음" ;;
         esac
